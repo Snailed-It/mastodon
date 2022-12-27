bc3d346f374ee93c2f05b39bc92eec8da4a3d79d
David Stephens <git@dwrs.org>
2022-12-27 20:29:28 +0000

    Change max status characters to be configurable (#12265)

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -5,7 +5,7 @@ class InitialStateSerializer < ActiveModel::Serializer
 
   attributes :meta, :compose, :accounts,
              :media_attachments, :settings,
-             :languages, :features
+             :languages, :features, :server
 
   attribute :critical_updates_pending, if: -> { object&.role&.can?(:view_devops) && SoftwareUpdate.check_enabled? }
 
@@ -90,6 +90,18 @@ class InitialStateSerializer < ActiveModel::Serializer
 
   def features
     Mastodon::Feature.enabled_features
+
+  def server
+    {
+      server: {
+        configuration:
+          {
+            statuses: {
+              max_characters: StatusLengthValidator.max_chars,
+            },
+          },
+      },
+    }
   end
 
   private
