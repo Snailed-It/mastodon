892efadaa1d8117c0c2d35083498d2ff3c9c733f
David Stephens <git@dwrs.org>
2022-12-29 16:39:26 +0000

    Log matched throttle name and remote ip

--- config/initializers/rack_attack_logging.rb
+++ config/initializers/rack_attack_logging.rb
@@ -3,6 +3,7 @@ ActiveSupport::Notifications.subscribe(/rack_attack/) do |_name, _start, _finish
 
   next unless [:throttle, :blacklist].include? req.env['rack.attack.match_type']
 
+  throttle_name = req.env['rack.attack.matched']
   discriminator = req.env['rack.attack.match_discriminator']
-  Rails.logger.info("Rate limit hit (#{req.env['rack.attack.match_type']}): #{discriminator} #{req.request_method} #{req.fullpath}")
+  Rails.logger.info("Rate limit hit (#{req.env['rack.attack.match_type']}:#{throttle_name}): #{discriminator} #{req.remote_ip} #{req.request_method} #{req.fullpath}")
 end
