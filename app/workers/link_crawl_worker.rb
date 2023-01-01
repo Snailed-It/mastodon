09a23ebae6093f1268cf62a3772b57500361ab1b
David Stephens <git@dwrs.org>
2023-01-01 15:27:56 +0000

    Change max delay before crawling links to be configurable from env
    # Conflicts:
    #	app/services/activitypub/process_status_update_service.rb

--- app/workers/link_crawl_worker.rb
+++ app/workers/link_crawl_worker.rb
@@ -3,6 +3,8 @@
 class LinkCrawlWorker
   include Sidekiq::Worker
 
+  MAX_DELAY = ENV.fetch('MAX_LINK_CRAWL_DELAY_SECONDS', 59).to_i
+
   sidekiq_options queue: 'pull', retry: 0
 
   def perform(status_id)
