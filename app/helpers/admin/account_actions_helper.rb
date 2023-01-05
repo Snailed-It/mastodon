6bf2fa9a4978c791a4b575a109cacf27b920a70c
David Stephens <git@dwrs.org>
2023-01-05 23:39:31 +0000

    Fix <strong> tag being escaped
    # Conflicts:
    #	app/views/admin/account_actions/new.html.haml

--- app/helpers/admin/account_actions_helper.rb
+++ app/helpers/admin/account_actions_helper.rb
@@ -5,7 +5,7 @@ module Admin::AccountActionsHelper
     safe_join(
       [
         I18n.t("simple_form.labels.admin_account_action.types.#{type}"),
-        content_tag(:span, I18n.t("simple_form.hints.admin_account_action.types.#{type}"), class: 'hint'),
+        content_tag(:span, raw(I18n.t("simple_form.hints.admin_account_action.types.#{type}")), class: 'hint'),
       ]
     )
   end
