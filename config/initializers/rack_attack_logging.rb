033ea88d25b59a086dd7e9ce3825a253c0214106
David Stephens <git@dwrs.org>
2023-01-08 22:09:36 +0000

    Only list IP in rate limit logs if it's different from discriminator

--- config/initializers/rack_attack_logging.rb
+++ config/initializers/rack_attack_logging.rb
@@ -5,5 +5,6 @@ ActiveSupport::Notifications.subscribe(/rack_attack/) do |_name, _start, _finish
 
   throttle_name = req.env['rack.attack.matched']
   discriminator = req.env['rack.attack.match_discriminator']
-  Rails.logger.info("Rate limit hit (#{req.env['rack.attack.match_type']}:#{throttle_name}): #{discriminator} #{req.remote_ip} #{req.request_method} #{req.fullpath}")
+  discriminator_segment = discriminator == req.remote_ip ? discriminator : "#{discriminator} #{req.remote_ip}"
+  Rails.logger.info("Rate limit hit (#{req.env['rack.attack.match_type']}:#{throttle_name}): #{discriminator_segment} #{req.request_method} #{req.fullpath}")
 end
