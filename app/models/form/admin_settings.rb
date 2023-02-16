c8c6580e20115801007067dedb34d94e5d65d149
David Stephens <git@dwrs.org>
2023-02-16 15:00:02 +0000

    Check if @status_max_chars exists before trying to access it

--- app/models/form/admin_settings.rb
+++ app/models/form/admin_settings.rb
@@ -84,7 +84,7 @@ class Form::AdminSettings
 
   define_model_callbacks :save
   before_save do
-    remove_instance_variable(:@status_max_chars) if @status_max_chars.blank?
+    remove_instance_variable(:@status_max_chars) if instance_variable_defined?("@status_max_chars") && @status_max_chars.blank?
     @status_max_chars = StatusLengthValidator::DEFAULT_MAX_CHARS if @status_max_chars == '0'
   end
 
